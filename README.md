f83cc674179b86552e1f19b05da0db7af7cb612d
Yu Arai <tenifre@gmail.com>
2013-02-13 22:57:58 +0900

    Add arguments option and change default bitrate

--- README.md
+++ README.md
@@ -3,3 +3,11 @@ flvToMp4
 
 Convert flv file to mp4 file all in the same level directory hierarchy.
 
+## Usage
+
+You can specify the bitrate by arguments.
+
+```
+  flv_to_mp4.sh {bitrate}
+```
+In the initial state, the bitrate is 800k.
--- flv_to_mp4.sh
+++ flv_to_mp4.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
+if [ -n $1 ]
+then
+  bitrate=$1
+else
+  bitrate="400k"
+fi
+
+mkdir flv
+
 for i in `ls *flv`
 do
-  ffmpeg -y -i ${i} -vcodec libx264 -b 800k -ac 2 -ar 44100 -ab 128k  `basename ${i} .flv`.mp4
+  ffmpeg -y -i ${i} -vcodec libx264 -b ${bitrate} -ac 2 -ar 44100 -ab 128k  `basename ${i} .flv`.mp4
+  mv ${i} flv
 done
