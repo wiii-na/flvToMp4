401485efd31abcd05b6e8996950797299eb3538d
Yu Arai <tenifre@gmail.com>
2013-02-18 23:21:45 +0900

    revise default option

--- flv_to_mp4.sh
+++ flv_to_mp4.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-if [ -n $1 ]
-then
+if [ $# -eq 1 ]; then
   bitrate=$1
 else
   bitrate="400k"
@@ -8,7 +7,7 @@ fi
 
 mkdir flv
 
-for i in `ls *flv`
+for i in `ls *.flv`
 do
   ffmpeg -y -i ${i} -vcodec libx264 -b ${bitrate} -ac 2 -ar 44100 -ab 128k  `basename ${i} .flv`.mp4
   mv ${i} flv
